592882da9d66a754d4f648ed1b4ecd1b0f030d58
Galih Rizki Setiadi <galihrizkisetiadi18@gmail.com>
2025-10-20 16:18:06 +0700

    fix: ui

--- pages/project.vue
+++ pages/project.vue
@@ -32,27 +32,54 @@ const projects = [
 </script>
 
 <template>
-    <div class="grid grid-cols-2 gap-8 py-[2rem] pr-[2rem] pl-[5rem]">
-        <a
-            v-for="(project, index) in projects"
-            :key="index"
-            :href="project.url"
-            target="_blank"
-            rel="noopener noreferrer"
-            :class="[
-                'group flex cursor-pointer flex-col gap-8 rounded-lg',
-                'bg-linear-65 from-gray-300 to-orange-200 dark:from-gray-900 dark:to-gray-500',
-            ]"
-        >
-            <div class="flex items-end justify-end pt-6 pr-4 text-2xl font-bold text-gray-700 dark:text-white">
-                {{ project.name }}
-            </div>
-            <img
-                :src="project.img"
-                :alt="project.name"
-                class="ml-[8rem] aspect-video rounded-xl group-hover:ml-0 hover:ml-0"
-                style="transition: margin-left 1s"
-            />
-        </a>
+    <div class="flex justify-center items-center">
+        <div class="grid-cols-2 gap-8 py-[2rem] pr-[2rem] pl-[5rem] hidden lg:grid">
+            <a
+                v-for="(project, index) in projects"
+                :key="index"
+                :href="project.url"
+                target="_blank"
+                rel="noopener noreferrer"
+                :class="[
+                    'group flex cursor-pointer flex-col gap-8 rounded-lg',
+                    'bg-linear-65 from-gray-300 to-orange-200 dark:from-gray-900 dark:to-gray-500',
+                    'h-fit w-fit',
+                ]"
+            >
+                <div class="flex items-end justify-end pt-6 pr-4 text-2xl font-bold text-gray-700 dark:text-white">
+                    {{ project.name }}
+                </div>
+                <img
+                    :src="project.img"
+                    :alt="project.name"
+                    class="ml-[8rem] aspect-video rounded-xl group-hover:ml-0 hover:ml-0"
+                    style="transition: margin-left 1s"
+                />
+            </a>
+        </div>
+
+        <div class="grid-cols-2 gap-8 py-[2rem] pr-[2rem] pl-[5rem] grid lg:hidden">
+            <a
+                v-for="(project, index) in projects"
+                :key="index"
+                :href="project.url"
+                target="_blank"
+                rel="noopener noreferrer"
+                :class="[
+                    'group flex cursor-pointer flex-col gap-8 rounded-lg',
+                    'bg-linear-65 from-gray-300 to-orange-200 dark:from-gray-900 dark:to-gray-500',
+                    'h-fit w-fit',
+                ]"
+            >
+                <div class="flex items-end justify-end pt-6 pr-4 text-2xl font-bold text-gray-700 dark:text-white">
+                    {{ project.name }}
+                </div>
+                <img
+                    :src="project.img"
+                    :alt="project.name"
+                    class="rounded-xl aspect-video m-4"
+                />
+            </a>
+        </div>
     </div>
 </template>
