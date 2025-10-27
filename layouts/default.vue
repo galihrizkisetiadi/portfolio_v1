8b9d14551603faa1635bac0286227dae0d9ff9b2
Galih Rizki Setiadi <galihrizkisetiadi18@gmail.com>
2025-10-27 13:32:17 +0700

    fix: color mode transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,10 +7,7 @@ const colorMode = useColorMode();
 
 <template>
     <div class="flex h-screen w-full items-end justify-end px-4 lg:px-40">
-        <div
-            class="h-[calc(92vh)] w-full rounded-t-lg lg:h-[calc(88vh)]"
-            :class="[{ 'bg-white': colorMode.value === 'light' }, { 'bg-black': colorMode.value === 'dark' }]"
-        >
+        <div class="card-paper h-[calc(92vh)] w-full rounded-t-lg lg:h-[calc(88vh)]">
             <Navbar />
             <div class="flex h-[calc(88vh-7rem)] gap-2 lg:gap-8">
                 <SideBar />
