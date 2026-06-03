c8179831ffda10126e85ccb9595eb4d96f30e357
Galih Rizki Setiadi <galihrizkisetiadi18@gmail.com>
2026-06-04 00:07:15 +0700

    fix: update cv

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ useHead({
 </script>
 
 <template>
-    <div class="flex items-center pr-4 lg:pr-40 pl-[5rem] lg:pl-[20rem] xl:pl-[15rem]">
+    <div class="flex items-center pr-4 pl-[5rem] lg:pr-40 lg:pl-[20rem] xl:pl-[15rem]">
         <div class="flex flex-col gap-16">
             <div class="flex w-fit flex-col gap-2 text-4xl font-bold lg:text-8xl">
                 <h1>Galih</h1>
@@ -18,7 +18,7 @@ useHead({
             </div>
             <div class="flex flex-col gap-4">
                 <p class="line-clamp-3 text-sm lg:w-[25rem] lg:text-2xl">{{ $t('index.desc') }}</p>
-                <a href="/CV-Galih_Rizki_Setiadi.pdf" download class="w-fit rounded-md border px-8 py-2 hover:bg-gray-200">CV</a>
+                <a href="/Galih Rizki Setiadi CV" download class="w-fit rounded-md border px-8 py-2 hover:bg-gray-200">CV</a>
             </div>
         </div>
     </div>
